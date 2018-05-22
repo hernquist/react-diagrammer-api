cf4252eb539d392b8a0f45bba0183fd2190929ab
David Hernquist <hernquistdavid@gmail.com>
2018-05-22 13:52:46 -0400

    cleaning up code for version

--- server/server.js
+++ server/server.js
@@ -1,22 +1,11 @@
 import express from "express";
-// import graphqlHTTP from "express-graphql";
-
 import schema from "./graphql/schema";
-
 import { graphqlExpress, graphiqlExpress } from "apollo-server-express";
 import morgan from "morgan";
 import bodyParser from "body-parser";
 
 const app = express(); 
 const dev = process.env.NODE_ENV === "development";
-
-// app.use(
-//   "/graphql",
-//   graphqlHTTP({
-//     schema,
-//     graphiql: dev
-//   })
-// );
 
 const homePath = '/graphiql';
 
@@ -49,25 +38,3 @@ const server = app.listen(3000, () => {
   const { port } = server.address();
   console.info(`\n\nExpress listen at http://localhost:${port} \n`);
 });
-
-// const homePath = '/graphiql';
-
-// app.use(
-//   homePath,
-//   graphiqlExpress({
-//     endpointURL: '/graphql'
-//   })
-// );
-
-// app.use('/graphql', 
-//   bodyParser.json(), 
-//   graphqlExpress( (req, res) => {
-//     return ({
-//       schema,
-//       context: { 
-//         SECRET, 
-//         user: req.user
-//       },
-//     })
-//   })
-// );
