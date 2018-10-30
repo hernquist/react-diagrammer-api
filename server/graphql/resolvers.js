c021ca751fd832de1431f7ebbd98009e4b976864
David Hernquist <david_hernquist@gmail.com>
2018-10-30 00:52:01 -0400

    check deleteComponent to make sure it is working

--- server/graphql/resolvers.js
+++ server/graphql/resolvers.js
@@ -146,10 +146,11 @@ export default {
       return result.n === 1;
     },
     deleteComponent: async (parent, { _id, parentId }, { Component }) => {
-      const { children } = await Component.find({ _id });
-      const updatedChildren = children.filter(id => id !== _id);
+      const component = await Component.find({ _id });
+      console.log(component[0]);
+      const updatedChildren = component[0].children.filter(id => id !== _id);
       const updatedParent = await Component.update(
-        { _id },
+        { _id: parentId },
         { children: updatedChildren }
       );
       console.log(updatedParent);
